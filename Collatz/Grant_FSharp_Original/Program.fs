e480ec3e130b57be9c0a211a562c480baa8ff7c4
Grant Crofton <grantac@hotmail.com>
2015-07-01 13:57:55 +0100

    Initial solution for Collatz

---
+++ Collatz/Grant_FSharp_Original/Program.fs
@@ -0,0 +1,13 @@
+﻿let isEven n = n % 2 = 0
+
+[<EntryPoint>]
+let main argv = 
+
+    let rec sequence = function
+        | 1 -> [1]
+        | n when isEven n -> n::sequence (n/2)
+        | n -> n::sequence (3 * n + 1)
+
+    let arg = int argv.[0]
+    printfn "%i" (sequence arg).Length
+    0
